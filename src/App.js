273869a23750ed70d197e3c5c3c3e218b4f181e9
MichaelMuya <michael.kimuri@strathmore.edu>
2024-01-28 20:27:12 +0300

    feat: add project structure

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import About from './pages/Experience';
-import Contact from '.pages/Contact';
-import Experience from '.pages/Experience';
-import Navbar from './Navbar';
-import Profile from '.pages/Profile';
+import Contact from './pages/Contact';
+import Experience from './pages/Experience';
+import Navbar from './pages/Navbar';
+import Profile from './pages/Profile';
 
 
 import React, {useState, useEffect} from 'react';
-import axios from 'axios'
 
 
 function App() {
-const [jokes, setJokes] = useState([])
-const [query, setQuery] = useState('teeth')
-
-useEffect(() => {
-  getResult();
-})
-
-const getResult = async () => {
-  const reponse = await axios
-  .get(`https://api.chucknorris.io/jokes/search?query=${query}`)
-  setJokes(reponse.data.result)
-}
-
 
   return (
       <div className='profile'>
---
+++ src/script.js
@@ -0,0 +1,7 @@
+function alt() {
+    const menu_link = document.querySelector(".menu_links");
+    const icon = document.querySelector(".icon_click");
+
+    menu_link.classList.toggle("open");
+    icon.classList.toggle("open");
+}
