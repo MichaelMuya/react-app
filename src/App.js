3f096f94325832b27c0496a6b9f666d02d438262
MichaelMuya <michael.kimuri@strathmore.edu>
2024-01-28 12:06:47 +0300

    feat: add project structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import About from './pages/Experience';
+import Contact from '.pages/Contact';
+import Experience from '.pages/Experience';
+import Navbar from './Navbar';
+import Profile from '.pages/Profile';
+
+
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
+
 
 function App() {
+const [jokes, setJokes] = useState([])
+const [query, setQuery] = useState('teeth')
+
+useEffect(() => {
+  getResult();
+})
+
+const getResult = async () => {
+  const reponse = await axios
+  .get(`https://api.chucknorris.io/jokes/search?query=${query}`)
+  setJokes(reponse.data.result)
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='profile'>
+        <Navbar />
+        <Profile />
+        <About />
+        <Experience />
+        <Contact />
+      </div>
   );
+  
 }
 
 export default App;
