5963a1b6ebe1d25ce1f961fb5a8cca03afa955d3
MichaelMuya <michael.kimuri@strathmore.edu>
2024-01-29 13:34:00 +0300

    feat: import styles

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+const  Navbar= () => {
+    return (  
+        <nav id="icon">
+        <div className="name">Michael Muya's Portfolio</div>
+        <div className="icon">
+            <div className="icon_click" onclick="alt()">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div className="menu_links">
+                <li><a href="#about" onclick="alt()">About</a></li>
+                <li><a href="#experience" onclick="alt()">Skills</a></li>
+                <li><a href="#contact" onclick="alt()">Contact</a></li>
+             </div>
+        </div>
+    </nav>
+    );
+}
+ 
+export default Navbar;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+const Profile = () => {
+    return ( 
+        <div className="section__text">
+          <h1 className="title">Michael Muya</h1>
+          <p>Software Developer</p>
+          <div id="socials-container">
+            <button><a href="https://github.com/MichaelMuya/" target="_blank">Github</a></button>
+            <button><a href="https://www.linkedin.com/in/michael-kimuri-868135242/" target="_blank">LinkedIn</a></button>
+          </div>
+        </div>
+     );
+}
+ 
+export default Profile;
