333998117d74edbcdc8275e7fc201ed792ec138b
MichaelMuya <michael.kimuri@strathmore.edu>
2024-01-29 15:32:28 +0300

    Updates

--- src/script.js
+++
@@ -1,7 +0,0 @@
-function alt() {
-    const menu_link = document.querySelector(".menu_links");
-    const icon = document.querySelector(".icon_click");
-
-    menu_link.classList.toggle("open");
-    icon.classList.toggle("open");
-}
